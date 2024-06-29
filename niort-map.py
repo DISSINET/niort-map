281aeba8fb8788bd4080521770eebc8e6fa573bc
tomas.hampejs <caerulus@gmail.com>
2024-06-29 10:04:32 +0200

    readme

--- niort-map.py
+++ niort-map.py
@@ -117,7 +117,7 @@ if "coords" in data.columns:
     map_data = []
 
     intro = """
-#  Map visualisation of the trial against Bernard Niort (1232)
+#  Map visualisation of the trial against Bernard Niort (1234-1235)
 
 Part of the talk "The Heresy Trial of Bernard-Othon de Niort and His Family, c. 1234-1235:
  Computing Discourses of Guilt at the Dawn of the Languedocian Inquisition" by
